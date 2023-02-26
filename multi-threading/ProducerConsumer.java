143bcd2cd4e53403f6b17ee010e11ee2952d3fab
Sethu Prathapan <sethuprathapanmuthirayil@gmail.com>
2023-02-27 00:27:25 +0530

    Create ProducerConsumer.java

---
+++ multi-threading/ProducerConsumer.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+public class ProducerConsumer{
+LinkedList<Integer> list=new LinkedList<>();
+public void produce() throws InterruptedException{
+int value=0;
+while(true){
+synchronized(this){
+while(list.size()>0)
+wait();
+System.out.println("Produced data: "+value);
+list.add(value);
+value++;
+notify();
+Thread.sleep(1000);
+}}}
+public void consume() throws InterruptedException{
+while(true){
+synchronized(this){
+while(list.size()==0)
+wait();
+int value=list.removeFirst();
+System.out.println("Consumed data: "+value);
+notify();
+Thread.sleep(1000);
+}}}
+public static void main(String args[]) throws InterruptedException{
+ProducerConsumer pc=new ProducerConsumer();
+Thread t1=new Thread(new Runnable(){
+public void run(){
+try{
+pc.produce();
+}
+catch(InterruptedException e){
+e.printStackTrace();}}});
+Thread t2=new Thread(new Runnable(){
+public void run(){
+try{
+pc.consume();
+}
+catch(InterruptedException e){
+e.printStackTrace();
+}}});
+t1.start();
+t2.start();
+t1.join();
+t2.join();}}
